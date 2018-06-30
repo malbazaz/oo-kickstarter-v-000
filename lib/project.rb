4568449d9ea2b6f179dc1dda707bf2cdfdcbecc7
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-30 19:09:46 +0000

    automatically backed up by learn

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+class Project
+  
+  attr_accessor :backers
+  attr_reader :title 
+ 
+  
+  def initialize(title)
+  @title = title 
+  @backers = []  
+  end 
+  
+  def back_project
+    
+  end 
+end 
